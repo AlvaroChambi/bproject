1eae6bea3aa34d9d330c9b3079eee90c836dc70b
AlvaroChambi <alvaro.chambi.campos@gmail.com>
2018-10-08 15:54:23 +0200

    added core framework

---
+++ app/src/main/java/es/developer/achambi/bproject/NeedListActivity.java
@@ -0,0 +1,19 @@
+package es.developer.achambi.bproject;
+
+import android.os.Bundle;
+
+import es.developer.achambi.coreframework.ui.BaseActivity;
+import es.developer.achambi.coreframework.ui.BaseFragment;
+
+public class NeedListActivity extends BaseActivity {
+
+    @Override
+    public int getScreenTitle() {
+        return R.string.app_name;
+    }
+
+    @Override
+    public BaseFragment getFragment(Bundle args) {
+        return new NeedListFragment();
+    }
+}
---
+++ app/src/main/java/es/developer/achambi/bproject/NeedListFragment.java
@@ -0,0 +1,36 @@
+package es.developer.achambi.bproject;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import es.developer.achambi.coreframework.ui.BaseSearchListFragment;
+import es.developer.achambi.coreframework.ui.SearchAdapterDecorator;
+import es.developer.achambi.coreframework.ui.presentation.SearchListData;
+
+public class NeedListFragment extends BaseSearchListFragment {
+
+    @Override
+    public SearchAdapterDecorator provideAdapter() {
+        return new SearchAdapterDecorator() {
+            @Override
+            public int getLayoutResource() {
+                return 0;
+            }
+
+            @Override
+            public RecyclerView.ViewHolder createViewHolder(View rootView) {
+                return null;
+            }
+
+            @Override
+            public void bindViewHolder(RecyclerView.ViewHolder holder, SearchListData item) {
+
+            }
+
+            @Override
+            public int getAdapterViewType() {
+                return 0;
+            }
+        };
+    }
+}
